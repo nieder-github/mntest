bcecd3af1d38d14f752df3d4af728691bc1cd090
Student User <student@workstation.lab.example.com>
2020-09-25 08:57:41 -0400

    update

--- mntest/src/main/java/mn/MnTest.java
+++ mntest/src/main/java/mn/MnTest.java
@@ -3,7 +3,13 @@ package mn;
 public class MnTest {
 
 public static void main(String args[]) {
-  System.out.println("MnTest");
+  while ( true) {
+    System.out.println("MnTest");
+    try {
+      Thread.sleep(10000);
+    } catch (Exception e) {
+    }
+  }
 }
 
 }
