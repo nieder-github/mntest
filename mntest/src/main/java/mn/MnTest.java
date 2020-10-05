ed19f0fa862271103e3e4752ece7e5c6eea1f64f
Student User <student@workstation.lab.example.com>
2020-10-05 04:16:37 -0400

    updated

--- mntest/src/main/java/mn/MnTest.java
+++ mntest/src/main/java/mn/MnTest.java
@@ -4,7 +4,7 @@ public class MnTest {
 
 public static void main(String args[]) {
   while ( true) {
-    System.out.println("MnTest");
+    System.out.println("MnTest updated");
     try {
       Thread.sleep(10000);
     } catch (Exception e) {
